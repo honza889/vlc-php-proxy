4c44accb45a72ecc30b3815a2a2566e1c470fbf8
root <root@HTVplayer.muh.local>
2014-05-09 19:20:20 +0200

    Default config

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-$CONFIG["vlc-http"]="http://localhost:8080";
-$CONFIG["vlc-stream"]="http://localhost:8081/stream.flv";
+$CONFIG["vlc-http"]="http://localhost:8080"; // this URL havent to be visible to user (only to this web app)
+$CONFIG["vlc-stream"]="http://localhost:8081/stream.flv"; // this URL must be visible to user (to web player work)
 $CONFIG["vlc-password"]="admin";
 
